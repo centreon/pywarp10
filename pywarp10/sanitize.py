00add0df1a2e27d473aead816416b816f6a8746a
Denis Roussel <droussel@centreon.com>
2024-12-16 09:11:55 -0500

    :bug: Invalid duration token was not properly sanitized (#35)
    Also fix all mypy errors.

--- pywarp10/sanitize.py
+++ pywarp10/sanitize.py
@@ -1,12 +1,12 @@
 import datetime
-from typing import Any, Iterable, Optional
+from functools import singledispatch
+from typing import Any, Optional
 
 import dateparser
 import durations  # type: ignore
-from durations.exceptions import ScaleFormatError  # type: ignore
+from durations.exceptions import InvalidTokenError, ScaleFormatError  # type: ignore
 
 import pywarp10.gts as gts
-from pywarp10 import GTS
 
 
 class SanitizeError(Exception):
@@ -27,6 +27,7 @@ class SanitizeError(Exception):
     pass
 
 
+@singledispatch
 def sanitize(x: Any) -> str | int:
     """Transforms python object into warpscript.
 
@@ -40,63 +41,84 @@ def sanitize(x: Any) -> str | int:
     Returns:
         A valid warpscript string.
     """
-    if isinstance(x, str):
-        if x.startswith("ws:"):
-            return x[3:]
-        try:
-            duration = durations.Duration(x).to_seconds()
-        except ScaleFormatError:
-            duration = 0
-        if duration > 0:
-            return int(duration * 1000000)
-        date = dateparser.parse(x, settings={"REQUIRE_PARTS": ["day", "month", "year"]})
-        if date is not None:
-            return int(date.timestamp() * 1000000)
-        return f"'{x}'"
-    if isinstance(x, bool):
-        return str(x).upper()
-    if isinstance(x, datetime.datetime):
-        # Someone may ask why I don't use isoformat() here like in the dateparser above.
-        # It's because dateparser can be ambiguous for some dates, so it's easier to
-        # check that the date was parsed correctly in logs if something went wrong.
-        # However, with datetime object, there is no ambiguity, and timestamp can be
-        # used directly, so warp10 won't have to transform it back itself.
-        return int(x.timestamp() * 1e6)
-    if isinstance(x, datetime.date):
-        # Date cannot be converted easily to a timestamp without making it a datetime.
-        x = datetime.datetime(x.year, x.month, x.day, tzinfo=datetime.timezone.utc)
-        return int(x.timestamp() * 1e6)
-    if isinstance(x, datetime.timedelta):
-        return int(x.total_seconds() * 1e6)
-    if isinstance(x, Iterable):
-        if isinstance(x, dict):
-            symbol_start = "{"
-            symbol_end = "}"
-        elif isinstance(x, list):
-            symbol_start = "["
-            symbol_end = "]"
-        else:
-            raise SanitizeError(x)
-        if len(x) == 0:
-            return symbol_start + symbol_end
-        separator = " "
-        indentation = ""
-        if len(str(x)) > 80:
-            separator = "\n"
-            indentation = " "
-        res = f"{symbol_start}{separator}"
-        if isinstance(x, dict):
-            for key, value in x.items():
-                res += f"{indentation}'{key}' {sanitize(value)}{separator}"
-        elif isinstance(x, list):
-            for value in x:
-                res += f"{indentation}{sanitize(value)}{separator}"
-        res += symbol_end
-        return res
+    raise SanitizeError(x)
+
+
+@sanitize.register
+def _(x: int) -> int:
     return x
 
 
-def desanitize(x: list[Any], bind_lgts=True) -> tuple | GTS | list[GTS]:
+@sanitize.register
+def _(x: str) -> str | int:
+    if x.startswith("ws:"):
+        return x[3:]
+    try:
+        duration = durations.Duration(x).to_seconds()
+    except (ScaleFormatError, InvalidTokenError):
+        duration = 0
+    if duration > 0:
+        return int(duration * 1000000)
+    date = dateparser.parse(x, settings={"REQUIRE_PARTS": ["day", "month", "year"]})
+    if date is not None:
+        return int(date.timestamp() * 1000000)
+    return f"'{x}'"
+
+
+@sanitize.register
+def _(x: bool) -> str:
+    return str(x).upper()
+
+
+@sanitize.register
+def _(x: datetime.datetime) -> int:
+    # Someone may ask why I don't use isoformat() here like in the dateparser above.
+    # It's because dateparser can be ambiguous for some dates, so it's easier to
+    # check that the date was parsed correctly in logs if something went wrong.
+    # However, with datetime object, there is no ambiguity, and timestamp can be
+    # used directly, so warp10 won't have to transform it back itself.
+    return int(x.timestamp() * 1e6)
+
+
+@sanitize.register
+def _(x: datetime.date) -> int:
+    # Date cannot be converted easily to a timestamp without making it a datetime.
+    x = datetime.datetime(x.year, x.month, x.day, tzinfo=datetime.timezone.utc)
+    return int(x.timestamp() * 1e6)
+
+
+@sanitize.register
+def _(x: datetime.timedelta) -> int:
+    return int(x.total_seconds() * 1e6)
+
+
+@sanitize.register
+def _(x: dict | list) -> str:
+    if isinstance(x, dict):
+        symbol_start = "{"
+        symbol_end = "}"
+    elif isinstance(x, list):
+        symbol_start = "["
+        symbol_end = "]"
+    if len(x) == 0:
+        return symbol_start + symbol_end
+    separator = " "
+    indentation = ""
+    if len(str(x)) > 80:
+        separator = "\n"
+        indentation = " "
+    res = f"{symbol_start}{separator}"
+    if isinstance(x, dict):
+        for key, value in x.items():
+            res += f"{indentation}'{key}' {sanitize(value)}{separator}"
+    elif isinstance(x, list):
+        for value in x:
+            res += f"{indentation}{sanitize(value)}{separator}"
+    res += symbol_end
+    return res
+
+
+def desanitize(x: list[Any], bind_lgts=True) -> tuple[Any] | gts.GTS | list[gts.GTS]:
     """Transforms a warpscript output into python object.
 
     Args:
@@ -110,7 +132,7 @@ def desanitize(x: list[Any], bind_lgts=True) -> tuple | GTS | list[GTS]:
 
     if gts.is_lgts(x):
         if not bind_lgts:
-            return [desanitize(g) for g in x]  # type: ignore
+            return [desanitize(g) for g in x]  # type: ignore # we know desanitize will return a GTS
         else:
             return gts.GTS(x)
 
