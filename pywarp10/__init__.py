b768204bf27463ee6ed951ef10209b50057b4ffb
Denis Roussel <droussel@centreon.com>
2025-05-14 09:23:33 -0400

    fix: update warning filters for date parsing and increment version to 0.3.5

--- pywarp10/__init__.py
+++ pywarp10/__init__.py
@@ -1,5 +1,14 @@
+import warnings
+
 from pywarp10.gts import GTS
 from pywarp10.pywarp10 import Warpscript
 from pywarp10.sanitize import desanitize, sanitize
 
-__version__ = "0.3.4"
+# Filter specific warnings
+warnings.filterwarnings(
+    "ignore",
+    category=DeprecationWarning,
+    message=".*Parsing dates involving a day of month without a year specified is ambiguious.*",  # noqa E501
+    module="dateparser.utils.strptime",
+)
+__version__ = "0.3.5"
